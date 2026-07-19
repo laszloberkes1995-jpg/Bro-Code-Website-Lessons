df8a0dad6e2a3ee35e608e57f9f6bc9be42d90b1
Laszlo Berkes <laszloberkes1995@gmail.com>
2026-07-19 23:02:53 +0100

    new lesson added

---
+++ JAVASCRIPT Lessons/Lesson34/index.js
@@ -0,0 +1,26 @@
+// .reduce() = reduce the elements of an array
+//             to a single value
+
+// const prices = [5, 30 ,10 ,25, 15 ,20];
+
+// const total = prices.reduce(sum);
+
+// console.log(`$${total.toFixed(2)}`);
+
+// function sum(accumulator, element){
+//     return accumulator + element;
+// }
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getmin);
+
+console.log(minimum);
+
+function getMax(accumulator, element){
+    return Math.max(accumulator,element);
+}
+function getmin(accumulator, element){
+    return Math.min(accumulator,element);
+}
