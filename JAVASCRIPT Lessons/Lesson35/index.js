fba4ffe9e04fb0a6e102fd0177ba3eb6e5eae1da
Laszlo Berkes <laszloberkes1995@gmail.com>
2026-07-24 11:23:28 +0100

    new lesson added

---
+++ JAVASCRIPT Lessons/Lesson35/index.js
@@ -0,0 +1,46 @@
+// function declaration = define a reusable block of code
+//                        that performs a specific task
+
+// function hello(){
+//     console.log("Hello");
+// }
+
+// function expressions = a way to define a function as
+//                        values or variables
+
+//  1. Callbacks in asynchronous operations
+//  2. Higher-Order Functions
+//  3. Clousers
+//  4. Event Listeners 
+
+
+// const hello = function(){
+//     console.log("hello");
+// }
+
+// hello();
+
+// setTimeout(function(){
+//     console.log("hello");
+// }, 3000);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map(function(element){
+    return Math.pow(element, 2);
+});
+const cubes = numbers.map(function(element){
+    return Math.pow(element, 3);
+})
+
+const evenNums = numbers.filter(function(element){
+    return element % 2 === 0;
+})
+const oddNums = numbers.filter(function(element){
+    return element % 2 !== 0;
+})
+
+const total = numbers.reduce(function(accumulator,element){
+    return accumulator + element;
+})
+console.log(total);
+
