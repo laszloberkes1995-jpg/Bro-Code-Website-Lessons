8cd6c37ca075bd2f46b5df0cc0d257b6a3bdbb55
Laszlo Berkes <laszloberkes1995@gmail.com>
2026-07-25 11:27:15 +0100

    new lesson added

---
+++ JAVASCRIPT Lessons/Lesson37/index.js
@@ -0,0 +1,36 @@
+// object = A collection of related properties and/or methods
+//          Can represent a real world objects (people, products, places)
+//          object = {key: value,
+//                    function()}
+
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
+    sayHello: () => console.log("Hi! I am Spongebob!"),
+    eat: () => console.log("I am eating Krabby Patty")
+}
+const person2 = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 42,
+    isEmployed: false,
+    sayHello: () => console.log("Hey, I'm Patrick..."),
+    eat: () => console.log("I am eating roast beef, chicken, and pizza")
+}
+
+// console.log(person1.firstName);
+// console.log(person1.lastName);
+// console.log(person1.age);
+// console.log(person1.isEmployed);
+
+// console.log(person2.firstName);
+// console.log(person2.lastName);
+// console.log(person2.age);
+// console.log(person2.isEmployed);
+
+// person1.sayHello();
+// person2.sayHello();
+person1.eat();
+person2.eat();
